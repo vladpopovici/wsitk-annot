c7fc616e6da0a303459e838563056368ee90d404
vladpopovici <popovici@bioxlab.org>
2024-12-18 15:07:44 +0100

    initial commit

---
+++ src/wsitk_annot/__init__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+#############################################################################
+# Copyright Vlad Popovici <popovici@bioxlab.org>
+#
+# Licensed under the MIT License. See LICENSE file in root folder.
+#############################################################################
+
+__author__ = "Vlad Popovici <popovici@bioxlab.org>"
+__version__ = 0.1
+
+from annot import (
+    AnnotationObject,
+    Dot,
+    Polygon,
+    PointSet,
+    Annotation,
+    Circle
+)
+
+__all__ = ['AnnotationObject', 'Dot', 'Polygon', 'PointSet', 'Annotation', 'Circle']
